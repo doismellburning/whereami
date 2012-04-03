e6c0203299944895ab761677e3fa91bd9c3e6a53
Kristian Glass <git@doismellburning.co.uk>
2012-04-03 16:03:20 +0100

    Make sure we set the right EMAIL_BACKEND when SendGrid is around!

--- whereami/whereami/settings.py
+++ whereami/whereami/settings.py
@@ -160,6 +160,7 @@ FOURSQUARE_PUSH_SECRET = os.getenv('FOURSQUARE_PUSH_SECRET')
 
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 if os.getenv('SENDGRID_USERNAME') is not None:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.sendgrid.net'
     EMAIL_HOST_USER = os.getenv('SENDGRID_USERNAME')
     EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
